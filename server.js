0c1b60c9f5021b654bdf2e8100c534c80a65311f
Porter Peterson <porterpeterson@Porters-MacBook-Air.local>
2024-02-14 21:49:00 -0700

    made express route

--- server.js
+++ server.js
@@ -2,6 +2,33 @@ const pg = require("pg");
 const client = new pg.Client(
   process.env.DATABASE_URL || "postgress://localhost/acme_hr_db"
 );
+const express = require("express");
+const app = express();
+
+app.get("/api/employees", async (req, res, next) => {
+  try {
+    const SQL = `  
+        SELECT * 
+        FROM employees
+        `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (ex) {
+    next(ex);
+  }
+});
+app.get("/api/departments", async (req, res, next) => {
+  try {
+    const SQL = `  
+          SELECT * 
+          FROM departments
+          `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 const init = async () => {
   await client.connect();
@@ -35,9 +62,18 @@ const init = async () => {
       INSERT INTO employees(txt, department_id) VALUES ('Doug', (
         SELECT id FROM departments WHERE name = 'BRAINSTORMING'
       ));
+      INSERT INTO employees(txt, department_id) VALUES ('Toby', (
+        SELECT id FROM departments WHERE name = 'HR'
+      ));
   `;
   await client.query(SQL);
   console.log("data seeded");
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
+
+  console.log("some curl commands to test");
+  console.log("curl localhost:3000/api/emplyees");
+  console.log("curl localhost:3000/api/departments");
 };
 
 init();
